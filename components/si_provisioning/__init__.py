529ab3ebe6d80660bcf953f3838191bd77928ff9
merv-gif <merv@riddsaqua.co.za>
2026-04-30 11:08:04 +0200

    Create __init__.py

---
+++ components/si_provisioning/__init__.py
@@ -0,0 +1,32 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["wifi", "mqtt", "globals"]
+AUTO_LOAD = ["json"]
+
+si_provisioning_ns = cg.esphome_ns.namespace("si_provisioning")
+SiProvisioning = si_provisioning_ns.class_("SiProvisioning", cg.Component)
+
+CONF_DEVICE_TYPE = "device_type"
+CONF_REGISTER_ENDPOINT = "register_endpoint"
+CONF_REGISTER_FINGERPRINT = "register_fingerprint"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(SiProvisioning),
+        cv.Required(CONF_DEVICE_TYPE): cv.one_of("pool", "water", lower=True),
+        cv.Required(CONF_REGISTER_ENDPOINT): cv.url,
+        cv.Required(CONF_REGISTER_FINGERPRINT): cv.string_strict,
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    cg.add(var.set_device_type(config[CONF_DEVICE_TYPE]))
+    cg.add(var.set_register_endpoint(config[CONF_REGISTER_ENDPOINT]))
+    cg.add(var.set_register_fingerprint(config[CONF_REGISTER_FINGERPRINT]))
+    cg.add_library("ESP Async WebServer", None)
+    cg.add_library("DNSServer", None)
