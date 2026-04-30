d8be1b1ec10158f893c14b7b40593b93fb73a6d6
merv-gif <merv@riddsaqua.co.za>
2026-04-30 12:04:27 +0200

    Update __init__.py

--- components/si_provisioning/__init__.py
+++ components/si_provisioning/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
+from esphome.core import CORE
 
 DEPENDENCIES = ["wifi", "mqtt", "globals"]
 AUTO_LOAD = ["json"]
@@ -25,5 +26,17 @@ async def to_code(config):
     await cg.register_component(var, config)
     cg.add(var.set_device_type(config[CONF_DEVICE_TYPE]))
     cg.add(var.set_register_endpoint(config[CONF_REGISTER_ENDPOINT]))
-    cg.add_library("ESP Async WebServer", None)
-    cg.add_library("DNSServer", None)
+
+    # Required Arduino-ESP32 framework libraries. Without these declarations,
+    # PlatformIO's LDF doesn't add the include paths for the bundled headers
+    # (WiFi.h, HTTPClient.h, etc.) and the build fails with
+    # "fatal error: WiFi.h: No such file or directory".
+    if CORE.using_arduino:
+        cg.add_library("FS", None)
+        cg.add_library("WiFi", None)
+        if CORE.is_esp32:
+            cg.add_library("HTTPClient", None)
+            cg.add_library("WiFiClientSecure", None)
+            cg.add_library("DNSServer", None)
+            cg.add_library("ESP Async WebServer", "3.7.7")
+            cg.add_library("AsyncTCP", "3.4.4")
